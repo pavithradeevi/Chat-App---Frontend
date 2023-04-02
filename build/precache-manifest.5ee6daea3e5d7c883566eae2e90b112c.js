21fe8eb43ee59f9a7f3ff961954545d0a6996f20
pavithra kamalakkannan <pavithradeevi@gmail.com>
2023-04-02 13:26:06 +0530

    first commit

---
+++ build/precache-manifest.5ee6daea3e5d7c883566eae2e90b112c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb28d56a18d574c9bf67904d14ed4b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bab26b07cf91f4e0051",
+    "url": "/static/css/main.7f74c50e.chunk.css"
+  },
+  {
+    "revision": "8968ff786c6ed0df2225",
+    "url": "/static/js/2.dd72232c.chunk.js"
+  },
+  {
+    "revision": "6bab26b07cf91f4e0051",
+    "url": "/static/js/main.113fce41.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
